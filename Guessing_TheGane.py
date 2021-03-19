e6f2b17fbfe1e29007f41f05617b945ad0220eaf
Yahia_Atif <natuya7ia.atif@gmail.com>
2021-03-19 19:51:59 +0200

    wait

---
+++ Guessing_TheGane.py
@@ -0,0 +1,14 @@
+# import random function
+from random import randrange
+
+#Introduction
+def intro():
+    print("**************************")
+    print("The game will choose a random number ")
+    print("berween 1 and 1000. you will be given ")
+    print("10 guesses. you start with 10 Points. Each ")
+    print("worng guess subracts a point. You can play")
+    print("As many tines as you like ")
+    print("***************************")
+    
+#intro()
